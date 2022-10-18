7fddc28a0b88aadf7c273ab7f9c0de9cced8fc47
Artyom <mortem@ngs.ru>
2022-10-18 13:19:21 +0700

    Практическая работа 6.9 Задание 3.

--- view/layout/header.php
+++ view/layout/header.php
@@ -29,14 +29,14 @@ require 'base/header.php';
 <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
         <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-        <span class="fs-4">Simple header</span>
+        <span class="fs-4"><?=$title?></span>
     </a>
 
     <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="/" class="nav-link active" aria-current="page">На главную</a></li>
         <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
         <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
         <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        <li class="nav-item"><a href="/about" class="nav-link">О нас</a></li>
     </ul>
 </header>
