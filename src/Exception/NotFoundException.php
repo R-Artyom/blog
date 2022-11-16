626a955bb729ef8993cc3e2d094dc1a213129a94
Artyom <mortem@ngs.ru>
2022-11-16 10:51:45 +0700

    1. Для наглядности и чтобы не запутаться шаблон errors.php переименован в 404.php 2. Ошибка. Если в директории view отсутствует файл шаблона, то не отображается текст ошибки "шаблон не найден". Исправлено

--- src/Exception/NotFoundException.php
+++ src/Exception/NotFoundException.php
@@ -14,7 +14,7 @@ class NotFoundException extends HttpException implements Renderable
         // Установить HTTP-статус ответа в значение 404 и заголовок ответа: HTTP/1.0 404 Not Found
         header("HTTP/1.0 404 Not Found");
         // Созание объекта-шаблона страницы "ошибка 404"
-        $error = new View('errors/errors', ['title' => 'Ошибка 404. Страница не найдена']);
+        $error = new View('errors/404', ['title' => 'Ошибка 404. Страница не найдена']);
         // Отображение шаблона данной страницы
         $error->render();
     }
