9dfbc493ea8411f1ef0099c2075ecbcfa2ef372a
Artyom <mortem@ngs.ru>
2022-11-29 21:15:18 +0700

    Теперь перед датой публикации комментария или статьи вставляется иконка "stopwatch" bootstrap в виде часов (https://icons.getbootstrap.com/icons/stopwatch/)

--- view/homepage.php
+++ view/homepage.php
@@ -4,7 +4,13 @@
             <div class="post row g-0 border shadow">
                 <div class="col p-4 position-relative link-style-3">
                     <p class="mb-1 h4"><?=$post->title?></p>
-                    <p class="mb-3 text-style-1 small">Опубликовано: <?=date('d.m.Y, H:i', strtotime($post->created_at))?></p>
+                    <p class="mb-3 text-style-1 small">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="watch" viewBox="0 0 16 16">
+                            <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"/>
+                            <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z"/>
+                        </svg>
+                        <?=date('d-m-Y, H:i', strtotime($post->created_at))?>
+                    </p>
                     <p class="mb-3"><?=$post->short_text?></p>
                     <p><a href="/posts/<?=$post->id?>" class="stretched-link link-style-1">Читать статью</a></p>
                 </div>
--- view/posts.php
+++ view/posts.php
@@ -1,7 +1,13 @@
 <div class="row g-0">
     <div class="row g-0 mb-4 px-5 py-4 border shadow">
         <p class="h4"><?=$post->title?></p>
-        <p class="mb-4 text-style-1 small"><?=date('d.m.Y H:i', strtotime($post->created_at))?></p>
+        <p class="mb-4 text-style-1 small">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="watch" viewBox="0 0 16 16">
+                <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"/>
+                <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z"/>
+            </svg>
+            <?=date('d-m-Y H:i', strtotime($post->created_at))?>
+        </p>
         <p class="row mb-4 g-0 justify-content-center">
             <img class="post-thumbnail shadow" src="/img/posts/<?=$post->img_name?>" alt="dots icon">
         </p>
@@ -44,11 +50,17 @@
                             <?php if ($comment->active === 0):?>
                                 <span class="position-absolute p-1 badge rounded-3 text-style-2 text-bg-style-3">Не проверено модератором</span>
                             <?php endif?>
-                            <div class="mb-2 text-style-1 small"><?='Зарегистрирован ' . date('d.m.Y [H:i:s]', strtotime($comment->user_created_at))?>
-                            <?='. Дата публикации ' . date('d.m.Y H:i', strtotime($comment->created_at))?></div>
+                            <div class="mb-2 text-style-1 small"><?=date('Зарегистрирован d-m-Y H:i', strtotime($comment->user_created_at))?></div>
                         </div>
-                        <div>
+                        <div class="mb-2">
                             <?=$comment->text?>
+                        </div>
+                        <div class="mb-2 text-style-1 small">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="watch" viewBox="0 0 16 16">
+                                <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"/>
+                                <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z"/>
+                            </svg>
+                            <?=date('d-m-Y H:i', strtotime($comment->created_at))?>
                         </div>
                     </div>
                 </div>
