3a24df542c10825ef92e371596c3b9bbd609eb22
Artyom <mortem@ngs.ru>
2022-12-01 16:35:59 +0700

    Теперь способ отображения иконок стал другой - при помощи иконочного шрифта bootstrap. А сам иконочный шрифт загружается через Сomposer

--- view/posts.php
+++ view/posts.php
@@ -2,10 +2,7 @@
     <div class="row g-0 mb-4 px-5 py-4 border shadow">
         <p class="h4"><?=$post->title?></p>
         <p class="mb-4 text-style-1 small">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stopwatch" viewBox="0 0 16 16">
-                <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"/>
-                <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z"/>
-            </svg>
+            <i class="bi bi-stopwatch"></i>
             <?=date('d-m-Y H:i', strtotime($post->created_at))?>
         </p>
         <p class="row mb-4 g-0 justify-content-center">
@@ -56,10 +53,7 @@
                             <?=$comment->text?>
                         </div>
                         <div class="mb-2 text-style-1 small">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stopwatch" viewBox="0 0 16 16">
-                                <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"/>
-                                <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z"/>
-                            </svg>
+                            <i class="bi bi-stopwatch"></i>
                             <?=date('d-m-Y H:i', strtotime($comment->created_at))?>
                         </div>
                     </div>
